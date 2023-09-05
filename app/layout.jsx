cb98de3b4c33c0a43ec7d16677cc6847709ccad1
Andi Adjie <achmdadjie@gmail.com>
2023-09-05 09:13:28 +0700

    add section tag for each components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,35 +1,42 @@
-import Navbar from './components/navbar/Navbar'
+import Navbar from "./components/navbar/Navbar";
 // import { Inter, Convergence } from 'next/font/google'
-import 'bootstrap/dist/css/bootstrap.css'
-import "./globals.css"
-import Headline from './components/head/Headline'
-import Description from './components/description/Description'
-import LatestProject from './components/latest-project/LatestProject'
+import "bootstrap/dist/css/bootstrap.css";
+import "./globals.css";
+import Headline from "./components/head/Headline";
+import Description from "./components/description/Description";
+import LatestProject from "./components/latest-project/LatestProject";
 // import CorsItem from './components/carousel-item/CarouselItem'
-import Footer from './components/footer/Footer'
+import Footer from "./components/footer/Footer";
 
-// const conv = Convergence({ 
+// const conv = Convergence({
 //   subsets: ['latin'],
 //   weight: "400",
 // })
 
 export const metadata = {
-  title: 'My Portfolio',
-  description: 'Creating my first portfolio',
-}
-
+  title: "My Portfolio",
+  description: "Creating my first portfolio",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        <Headline />
-        <Description />
-        <LatestProject />
+        <section>
+          <Navbar />
+          <Headline />
+        </section>
+        <section>
+          <Description />
+        </section>
+        <section>
+          <LatestProject />
+        </section>
+        <section>
+          <Footer />
+        </section>
         {/* <CorsItem /> */}
-        <Footer />
       </body>
     </html>
-  )
+  );
 }
