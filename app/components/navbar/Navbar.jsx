b255eca7eb5c0365e347ddb1bb11efb65fc39bdb
Andi Adjie <achmdadjie@gmail.com>
2023-09-06 07:31:20 +0700

    add link to home in the logo

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import Image from "next/image";
-// import Link from "next/link";
+import Link from "next/link";
 import logo from "../../../public/logo.svg";
 import { Convergence } from "next/font/google";
 
@@ -14,7 +14,7 @@ function Navbar() {
   return (
     <nav className={conv.className}>
       <div className="d-flex justify-content-between bg-color px-5">
-        <Image className="logo" src={logo} alt="logo-img" />
+        <Link href={"/"}><Image className="logo" src={logo} alt="logo-img" /></Link>
         <div className="d-flex align-items-center column-gap-4">
           <a href="#desc" className="font-color">
             About
