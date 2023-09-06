2b38d1921fe130059397c7646b5addeb3b14fa85
Andi Adjie <achmdadjie@gmail.com>
2023-09-06 08:35:12 +0700

    change the width and height of the container

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -12,19 +12,21 @@ const conv = Convergence({
 
 function Navbar() {
   return (
-    <nav className={conv.className}>
-      <div className="d-flex justify-content-between bg-color px-5">
-        <Link href={"/"}><Image className="logo" src={logo} alt="logo-img" /></Link>
-        <div className="d-flex align-items-center column-gap-4">
-          <a href="#desc" className="font-color">
-            About
-          </a>
-          <a href="#footer" className="border-1 rounded-pill px-3 py-2 color">
-            Say Hello
-          </a>
-        </div>
+    <div
+      className={`d-flex justify-content-between bg-color px-5 ${conv.className}`}
+    >
+      <Link href={"/"}>
+        <Image className="logo" src={logo} alt="logo-img" />
+      </Link>
+      <div className="d-flex align-items-center column-gap-4">
+        <a href="#desc" className="font-color">
+          About
+        </a>
+        <a href="#footer" className="border-1 rounded-pill px-3 py-2 color">
+          Say Hello
+        </a>
       </div>
-    </nav>
+    </div>
   );
 }
 
